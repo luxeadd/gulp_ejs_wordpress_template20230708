8c508716117ddd8409a4366d806b1efbe8417aed
sassy <luxeadd@gmail.com>
2024-05-10 10:27:29 +0900

    picture見直し

--- src/WordPress/tmp/picture.php
+++ src/WordPress/tmp/picture.php
@@ -1,9 +1,9 @@
 <picture>
   <?php if ($args['spImg'] !== '') : ?>
-    <source srcset="<?php echo my_image_path() . $args['pictureImg'] . $args['spImgName'] ?>.webp" <?php if ($args['spWidth'] !== '') : ?>width="<?php echo $args['spWidth'] ?>" <?php endif; ?> <?php if ($args['spHeight'] !== '') : ?>height="<?php echo $args['spHeight'] ?>" <?php endif; ?> media="(max-width:767px)" type="image/webp">
     <?php if ($args['webp'] === 'true') : ?>
-      <source srcset="<?php echo my_image_path() . $args['pictureImg'] . $args['spImgName'] . $args['file'] ?>" <?php if ($args['spWidth'] !== '') : ?>width="<?php echo $args['spWidth'] ?>" <?php endif; ?> <?php if ($args['spHeight'] !== '') : ?>height="<?php echo $args['spHeight'] ?>" <?php endif; ?> media="(max-width:767px)">
+      <source srcset="<?php echo my_image_path() . $args['pictureImg'] . $args['spImgName'] ?>.webp" <?php if ($args['spWidth'] !== '') : ?>width="<?php echo $args['spWidth'] ?>" <?php endif; ?> <?php if ($args['spHeight'] !== '') : ?>height="<?php echo $args['spHeight'] ?>" <?php endif; ?> media="(max-width:767px)" type="image/webp">
     <?php endif; ?>
+    <source srcset="<?php echo my_image_path() . $args['pictureImg'] . $args['spImgName'] . $args['file'] ?>" <?php if ($args['spWidth'] !== '') : ?>width="<?php echo $args['spWidth'] ?>" <?php endif; ?> <?php if ($args['spHeight'] !== '') : ?>height="<?php echo $args['spHeight'] ?>" <?php endif; ?> media="(max-width:767px)">
   <?php endif; ?>
   <?php if ($args['webp'] === 'true') : ?>
     <source srcset="<?php echo my_image_path() . $args['pictureImg'] ?>.webp" <?php if ($args['pcWidth'] !== '') : ?>width="<?php echo $args['pcWidth'] ?>" <?php endif; ?> <?php if ($args['pcHeight'] !== '') : ?>height="<?php echo $args['pcHeight'] ?>" <?php endif; ?> type="image/webp">
